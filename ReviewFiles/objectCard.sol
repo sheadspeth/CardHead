3da033270e265ffb0fa69f5f1b28003e94ea1599
Iman Abboud <abboud.iman@gmail.com>
2025-04-21 00:53:12 -0700

    420 Changes

--- ReviewFiles/objectCard.sol
+++ ReviewFiles/objectCard.sol
@@ -16,11 +16,22 @@ contract objectCard {
         string imageBack;
     }
 
+    struct Certify {
+        uint barcode;
+        address user;
+        uint8 rating;
+    }
+
+    uint private nextBarcode = 1;
+
     mapping(uint256 => Card) public cardDataBase;
+    Certify[] public certifiedCards;
+
+    event CardAdded(uint barcode, string name);
+    event CardCertified(uint barcode, address user, uint8 rating);
 
     // Add a new card to the database
     function addCard(
-        uint _barcode,
         string memory _name,
         string memory _game,
         string memory _series,
@@ -32,17 +43,23 @@ contract objectCard {
     ) public {
         require(_rating <= 10, "Rating must be between 0 and 10");
 
-        cardDataBase[_barcode] = Card({
+        uint currentBarcode = nextBarcode;
+
+        cardDataBase[currentBarcode] = Card({
             game: _game,
             series: _series,
             name: _name,
             grade: _grade,
             edition: _edition,
-            barcode: _barcode,
+            barcode: currentBarcode,
             rating: _rating,
             imageFront: _imageFront,
             imageBack: _imageBack
         });
+
+        emit CardAdded(currentBarcode, _name);
+
+        nextBarcode++;
     }
 
     function getCard(uint _barcode) public view returns (
@@ -58,4 +75,38 @@ contract objectCard {
         Card memory c = cardDataBase[_barcode];
         return (c.name, c.game, c.series, c.grade, c.edition, c.rating, c.imageFront, c.imageBack);
     }
+
+    function getNextBarcode() public view returns (uint) {
+        return nextBarcode;
+    }
+
+    function cardCertification(uint _barcode, uint8 _rating) public {
+        require(_barcode > 0 && _barcode < nextBarcode,"Card does not exist");
+        require(_rating <= 10,"Rating must be between 0 and 10");
+
+        certifiedCards.push(Certify({
+            barcode: _barcode,
+            user: msg.sender,
+            rating: _rating
+        }));
+
+        emit CardCertified(_barcode, msg.sender, _rating);
+    }
+
+    
+    function getCertifiedCard(uint index) public view returns (
+        uint barcode,
+        address user,
+        uint8 rating
+    ) {
+        require(index < certifiedCards.length, "Invalid index");
+        Certify memory cert = certifiedCards[index];
+        return (cert.barcode, cert.user, cert.rating);
+    }
+
+    
+    function getCertifiedCardCount() public view returns (uint) {
+        return certifiedCards.length;
+    }
 }
+
--- ReviewFiles/objectCardHead.sol
+++ ReviewFiles/objectCardHead.sol
@@ -3,12 +3,26 @@ pragma solidity ^0.8.0;
 
 contract objectCardHead {
 
+    address public marketplace;
+    address public owner;
+
     struct User{
         string name;
         uint[] ownedCardIds;
     }
 
     mapping(address => User) public users;
+
+    event Debug(string note, address caller, address from, address to, uint cardID);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setMarketplace(address _marketplace) public {
+        require(msg.sender == owner, "Only owner can set marketplace");
+        marketplace = _marketplace;
+    }
 
     function createUser(string memory _name) public {
         require(bytes(users[msg.sender].name).length == 0, "User already exists");
@@ -26,5 +40,26 @@ contract objectCardHead {
 
     function getMyName() public view returns (string memory) {
         return users[msg.sender].name;
+    }
+
+    function transferCardOwnership(address from, address to, uint cardID) public {
+        emit Debug("Entered transferCardOwnership", msg.sender, from, to, cardID);
+        require(msg.sender == marketplace, "Only marketplace can transfer");
+
+            uint[] storage fromCards = users[from].ownedCardIds;
+    bool found = false;
+
+    for (uint i = 0; i < fromCards.length; i++) {
+        if (fromCards[i] == cardID) {
+            fromCards[i] = fromCards[fromCards.length - 1]; // move last item to this index
+            fromCards.pop(); // remove last item
+            found = true;
+            break;
+        }
+    }
+
+    require(found, "Card not found in sender's inventory");
+
+        users[to].ownedCardIds.push(cardID);
     }
 }
